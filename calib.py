80db8a30c3b2792220461c401df3613aabfb7754
JuneshG <157029875+JuneshG@users.noreply.github.com>
2024-01-29 15:45:47 -0600

    Add files via upload

--- calib.py
+++ calib.py
@@ -2,24 +2,21 @@ import numpy as np
 import cv2 as cv
 import glob
 
-
-
 # termination criteria
-criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+criteria      = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*5,3), np.float32)
-objp[:,:2] = np.mgrid[0:6,0:5].T.reshape(-1,2)
+objp          = np.zeros((6*5,3), np.float32)
+objp[:,:2]    = np.mgrid[0:6,0:5].T.reshape(-1,2)
 # Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
+objpoints     = [] # 3d point in real world space
+imgpoints     = [] # 2d points in image plane.
 images = glob.glob('*.jpg')
-
 
 for fname in images:
 
     print( "reading filename ", fname )
-    img = cv.imread(fname)
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    img          = cv.imread(fname)
+    gray         = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     # Find the chess board corners
     ret, corners = cv.findChessboardCorners(gray, (6,5), None)
     # If found, add object points, image points (after refining them)
@@ -32,9 +29,9 @@ for fname in images:
         cv.imshow('img', img)
         cv.waitKey(500)
 
-
 cv.destroyAllWindows()
 
 ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
-print( " Camera Matrix, distortion matrix ", mtx, dist)
+print("camera matrix : ", mtx)
+print("distortion matrix: ", dist)
